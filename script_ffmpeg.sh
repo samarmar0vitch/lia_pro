7ae1a9db592291bce4f671b0f322e7f13a439fe5
l00ke3 <71680654+l00ke3@users.noreply.github.com>
2023-08-24 15:27:36 +0100

    Update script_ffmpeg.sh

--- script_ffmpeg.sh
+++ script_ffmpeg.sh
@@ -15,7 +15,7 @@ ffmpeg  -i $u \
 -c:v libx264 -b:a 384k -ac 2 -preset slow -crf 28 \
 -profile:v high -bf 2 -pix_fmt yuv420p \
 -threads 4 -xerror \
--vstats_file MFRfile.txt -maxrate 5M -bufsize 10M -r 30 -g 15 -coder 1 -f flv $key_stream #2> ylog.txt
+-maxrate 5M -bufsize 10M -r 30 -g 15 -coder 1 -f flv $key_stream #2> ylog.txt
 
     sleep 1
 done
