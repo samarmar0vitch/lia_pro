9b9c1ee5360f8bc28f6b1253be457461e8a84de8
l00ke3 <71680654+l00ke3@users.noreply.github.com>
2023-08-24 15:27:04 +0100

    Update script_ffmpeg.sh

--- script_ffmpeg.sh
+++ script_ffmpeg.sh
@@ -1,16 +1,6 @@
 #!/bin/bash
 u=$1
-#https://pull-flv-l11-va01.tiktokcdn.com/stage/stream-2995889464288477225_ld.flv"
-#u="https://pull-flv-l11-va01.tiktokcdn.com/stage/stream-2995889464288477225_or4.flv"
-#u="https://pull-flv-l10-sg01.tiktokcdn.com/stage/stream-2131194588457074889_or4.flv"
-#"https://pull-flv-l11-sg01.tiktokcdn.com/stage/stream-2131194051112730667_or4.flv"
-#u="https://pull-flv-l11-va01.tiktokcdn.com/stage/stream-2995884191939821609_or4.flv"
-#u="https://pull-flv-l1-sg01.tiktokcdn.com/stage/stream-2419421850622230574_or4.flv"
-#https://pull-flv-l11-sg01.tiktokcdn.com/stage/stream-2419421421996343339_or4.flv"
-#https://pull-flv-l11-sg01.tiktokcdn.com/stage/stream-2419419025472749611_uhd.flv"
-#"https://pull-flv-l11-va01.tiktokcdn.com/stage/stream-2995880381150920745_or4.flv"
-#url_tik="https://pull-flv-l11-sg01.tiktokcdn.com/stage/stream-2419419712398032939_or4.flv"
-key_stream="rtmp://a.rtmp.youtube.com/live2/ms9z-mx5s-fvjg-9tmf-93ap"
+key_stream="rtmp://a.rtmp.youtube.com/live2/"$you_tokens
 trap printout SIGINT
 printout() {
     echo ""
