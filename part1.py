72233d55512b7f7e6466782c2fd160cc5ea3b38b
l00ke3 <71680654+l00ke3@users.noreply.github.com>
2023-09-19 00:20:41 +0100

    Update part1.py

--- part1.py
+++ part1.py
@@ -21,7 +21,7 @@ def ffmpeg_fire_up(stream_url):
         
         try:
                 pcmd = "./script_ffmpeg.sh "+stream_url
-                sys_notification.send_it(lol)
+                #sys_notification.send_it(lol)
                 args = pcmd.split()
                 process = subprocess.Popen(args, stdout=subprocess.PIPE, stderr=subprocess.STDOUT,universal_newlines=True)
                 for line in process.stdout:
